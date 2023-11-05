7d15355b3850a635733baaad19c03908d1b31a02
Subin <sub_1n@yahoo.com>
2023-11-05 23:00:13 +0200

    lodash functions

--- tryModules.js
+++ tryModules.js
@@ -20,9 +20,25 @@ import _ from "lodash";
 // greet();
 // greet();
 
-// Lodash provides functions to iterate over arrays
+// ? Lodash provides functions to iterate over arrays
 const numbers = [1, 2, 3, 4, 5];
-_.forEach(numbers, (num) => console.log(num));
-_.map(numbers, (num) => console.log(Math.pow(num, 2)));
-const filteredArr = _.filter(numbers, (num) => num % 2 == 0);
-console.log(filteredArr);
+// _.forEach(numbers, (num) => console.log(num));
+// _.map(numbers, (num) => console.log(Math.pow(num, 2)));
+// const filteredArr = _.filter(numbers, (num) => num % 2 == 0);
+// console.log(filteredArr);
+
+// ? We can merge objects using _.merge or _.assign
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, d: 4 };
+
+// const mergedObject = _.merge({}, obj1, obj2);
+// console.log(mergedObject);
+
+// ? lodash chaining Methods => Chaining allows you to perform a sequence of operations on a data set in a clean and readable way. You can chain together multiple Lodash methods and then call .value() to execute the chain.
+
+// in the example below lodash is chaining the results from map function to filter and to value which gives the final result
+const result = _.chain(numbers)
+  .map((num) => num * 2)
+  .filter((num) => num % 2 === 0)
+  .value();
+console.log(result);
