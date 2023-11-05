d45812f358dae8a63ddc8054b58fec231f116167
Subin <sub_1n@yahoo.com>
2023-11-05 22:07:12 +0200

    node modules lodash

--- tryModules.js
+++ tryModules.js
@@ -1,5 +1,11 @@
 import os from "os";
-console.log(os.version());
-console.log(os.platform());
-console.log(os.release());
-console.log(os.type());
+//const _ = require("lodash");
+import _ from "lodash";
+
+// console.log(os.version());
+// console.log(os.platform());
+// console.log(os.release());
+// console.log(os.type());
+
+const num = _.random(0, 20);
+console.log(num);
