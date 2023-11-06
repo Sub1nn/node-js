cfd1ea1bf36a05e6b3c3a0844096fe7798e63ad7
Subin <sub_1n@yahoo.com>
2023-11-06 23:13:02 +0200

    node modules practice

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import { Express } from "express";
+//import { Express } from "express";
+import chalk from "chalk";
+
+const log = console.log;
+
+log(chalk.green("Hello") + chalk.red(" World") + "!");
--- tryInquirer.js
+++ tryInquirer.js
@@ -1,15 +1,46 @@
 import inquirer from "inquirer";
+//import { result } from "lodash";
 
-const questions = [
-  {
-    type: "input",
-    name: "num",
-    message: "Enter number",
-  },
-];
+// const questions = [
+//   {
+//     type: "input",
+//     name: "num",
+//     message: "Enter number",
+//   },
+// ];
 
-inquirer.prompt(questions).then((answers) => {
-  let result;
-  answers.name % 2 === 0 ? (result = "even") : (result = "odd");
-  console.log(result);
-});
+// inquirer.prompt(questions).then((answers) => {
+//   let result;
+//   answers.name % 2 === 0 ? (result = "even") : (result = "odd");
+//   console.log(result);
+// });
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "num1",
+      message: "Enter a number\n",
+    },
+    {
+      type: "input",
+      name: "num2",
+      message: "Enter second number\n",
+    },
+    {
+      type: "input",
+      name: "string",
+      message: "Tell either add, sub or multiply\n",
+    },
+  ])
+  .then((ans) => {
+    let result = 0;
+    if (ans.string === "add") {
+      result = +ans.num1 + +ans.num2;
+    } else if (ans.string === "sub") {
+      result = +ans.num1 - +ans.num2;
+    } else {
+      result = +ans.num1 * +ans.num2;
+    }
+    console.log(result);
+  });
