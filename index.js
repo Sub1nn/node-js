cb626f142e235d759c634f22ead31a8d746bc5b1
Subin <sub_1n@yahoo.com>
2023-11-06 18:45:59 +0200

    gitignore added and node practice

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import inquirer from "inquirer";
-
-const questions = [
-  {
-    type: "input",
-    name: "num",
-    message: "Enter number",
-  },
-];
-
-inquirer.prompt(questions).then((answers) => {
-  let result;
-  answers.name % 2 === 0 ? (result = "even") : (result = "odd");
-  console.log(result);
-});
+import { Express } from "express";
