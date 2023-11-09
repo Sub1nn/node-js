e4416fbf7dbd683076c2ffd177bde82e384f6468
Subin <sub_1n@yahoo.com>
2023-11-09 21:26:50 +0200

    customer route updated

--- customer.routes.js
+++ customer.routes.js
@@ -12,8 +12,61 @@ let customers = [
     address: "Koteswor",
   },
 ];
-router.get("/customer/list", (req, res) => {
-  console.log(customers);
+router.get("/customers/list", (req, res) => {
+  return res.status(201).send(customers);
+});
+
+router.post("/customers/add", (req, res) => {
+  const addedCustomer = req.body;
+  customers.push(addedCustomer);
+  return res.status(200).send({ message: "customer added successfully" });
+});
+
+router.delete("/customers/delete/:id", (req, res) => {
+  const customerToBeDeleted = +req.params.id;
+  const customer = customers.find((item) => {
+    return item.id === customerToBeDeleted;
+  });
+  if (!customer) {
+    res.status(404).send({
+      message: `Customer with the id ${customerToBeDeleted} was not found`,
+    });
+  }
+  const newCustomerList = customers.filter((item) => {
+    return item.id !== customerToBeDeleted;
+  });
+
+  customers = [...newCustomerList];
+
+  return res.status(200).send({ message: "customer removed successfully" });
+});
+
+router.put("/customers/edit/:id", (req, res) => {
+  const updatedCustomer = req.body;
+  console.log(req.body);
+  const customerToBeUpdated = +req.params.id;
+  const customer = customers.find((item) => {
+    return item.id === customerToBeUpdated;
+  });
+  console.log(customer);
+  if (!customer) {
+    return res.status(404).send({
+      message: `Customer with the id ${customerToBeUpdated} was not found`,
+    });
+  }
+
+  const newCustomerList = customers.map((item) => {
+    if (item.id == customerToBeUpdated) {
+      item = { ...item, ...updatedCustomer };
+    }
+    return item;
+  });
+
+  customers = [...newCustomerList];
+
+  return res.status(200).send({
+    message: `Customer with the id ${customerToBeUpdated} updated successfully`,
+  });
 });
 
 export default router;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import laptopRoutes from "./laptop.routes.js";
+import customerRoutes from "./customer.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(laptopRoutes);
+app.use(customerRoutes);
 
 const port = 8000;
 app.listen(port, () => {
